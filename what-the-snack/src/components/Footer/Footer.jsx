ee605c2ab0481c6ba25ce21aef6bf8ac90ac87c0
shinheeje <dbfk2637@naver.com>
2023-04-28 22:54:59 +0900

    Feat: 헤더, 푸터 레이아웃완료 및 메인페이지 레이아웃 제작

--- what-the-snack/src/components/Footer/Footer.jsx
+++ what-the-snack/src/components/Footer/Footer.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
-
+import styled from 'styled-components'
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <FooterContainer>
+        <FooterLeftContent>
+          <p>© Bucheon neighborhood 2023</p>
+          <p>부천신중동역2번출구(유) 대표: Heeje, Hansol, 부천시 오정구 소사대로 382 (도당동) 항해99타워, 01499</p>
+          <p>사업자등록번호: 131-41-41799</p>
+          <p>사업자정보조회</p>
+          <p>고객관리지원팀: (02) 8941-0234 lv4test@gamil.com</p>
+        </FooterLeftContent>
+        <Logo src={`${process.env.PUBLIC_URL}/images/logo-removebg-preview.png`} />
+        <FooterRightContent>
+          <FotterName>Shin Hee Je</FotterName>
+          <FotterName>Kim Han Sol</FotterName>
+        </FooterRightContent>
+      </FooterContainer>
+    </>
   )
 }
 
+const FooterContainer = styled.footer`
+  height: 100px;
+  padding: 10px 30PX 10PX 30PX;
+  background-color: #ff4429;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 10px;
+  /* color: rgba(255, 255, 255, 0.9); */
+  color: black;
+  border-top: 2px solid black;
+
+`
+
+const FooterLeftContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 5px;
+  @media (max-width:1080px) {
+    display:none;
+    transition: all 0.5s;
+  }
+`
+const Logo = styled.img`
+  width: 100px;
+  transform: translateX(-50%);
+  @media (max-width:1080px) {
+    width: 130px;
+    transform: translateX(0);
+    transition: all 0.5s;
+  }
+`
+
+const FooterRightContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+
+const FotterName = styled.p`
+  font-size: 30px;
+    font-family: 'Libre Barcode 39 Text', cursive;
+`
 export default Footer
