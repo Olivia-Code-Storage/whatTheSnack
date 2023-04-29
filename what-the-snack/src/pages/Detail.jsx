376bf7c87230b1bb903bd3bdb5f2d8174e0083e3
shinheeje <dbfk2637@naver.com>
2023-04-29 11:43:38 +0900

    Feat: 헤더 푸터 레이아웃 수정

--- what-the-snack/src/pages/Detail.jsx
+++ what-the-snack/src/pages/Detail.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import styled from 'styled-components';
 
 const Detail = () => {
   return (
-    <div>과자 추천 상세 페이지</div>
+    <>
+      <Header />
+      <DetailContainer>
+
+        <DetailContentImg src={`${process.env.PUBLIC_URL}/images/haru.png`} alt="" />
+
+        <div>
+
+          <h3>제목</h3>
+
+          <h4>작성자</h4>
+
+          <p>What the Snack exists for those who cherish their daily routines and aspire to make each day perfect, <br />
+            willingly embracing moments of rest and relaxation as a means of personal growth. <br />
+            It's the joy of discovering someone else's favorite treat, <br /></p>
+          <p />
+
+          <p>
+
+          </p>
+          
+        </div>
+
+      </DetailContainer >
+      <Footer />
+    </>
   )
 }
 
+
+const DetailContainer = styled.div`
+background-color: #002e80;
+  height: 537px;
+  display: flex;
+`
+
+const DetailContentImg = styled.img`
+  width: 800px;
+  height: 500px;
+  border: 2px solid white;
+`
 export default Detail
